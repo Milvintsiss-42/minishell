58a4bc3970e4b825fd3e3566a1628dab220e1328
Milvintsiss <milvintsiss@gmail.com>
2022-08-30 15:57:48 +0200

    Adds append_mode toggle and documentation for command parsing

--- includes/common.h
+++ includes/common.h
@@ -6,13 +6,22 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 02:36:55 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/08/30 02:41:45 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/08/30 15:46:39 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMON_H
 # define COMMON_H
 
+// Only last infile, outfile, or here_doc are needed, write an error to the
+// stderr for each infile, outfile, here_doc who are overwrote.
+// ex: cat < infile << HERE > outfile >> outfile2 | cat
+// -> write an error for the first infile
+// -> here_doc is passed as stdin
+// -> write an error for the second outfile
+// -> stdout of cat is written to outfile2 in append mode
+// -> nothing is passed as stdin to the second "cat" but "cat" doesn't wait for
+// input.
 typedef struct s_command
 {
 	const char	*cmd;
@@ -22,6 +31,7 @@ typedef struct s_command
 	const char	*here_doc_limiter;
 	const char	*infile;
 	const char	*outfile;
+	int			is_append_mode;
 	int			pipe_in;
 	int			pipe_out;
 	int			is_last;
