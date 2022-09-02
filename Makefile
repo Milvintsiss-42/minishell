Milvintsiss-42/minishell

commit a418148ba9835c208efe5cce441daab503074257

Adds debug flags

Makefile
@@ -6,7 +6,7 @@
 #    By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/01/29 15:36:23 by ple-stra          #+#    #+#              #
-#    Updated: 2022/09/01 19:35:25 by ple-stra         ###   ########.fr        #
+#    Updated: 2022/09/02 15:13:04 by ple-stra         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -35,6 +35,9 @@ ifeq (sanitize, $(filter sanitize,$(MAKECMDGOALS)))
 endif
 ifeq (debug, $(filter debug,$(MAKECMDGOALS)))
 	CFLAGS	+= -D KDEBUG=1
+endif
+ifeq (debug_execution, $(filter debug_execution,$(MAKECMDGOALS)))
+	CFLAGS	+= -D KDEBUG_EXECUTION=1
 endif
 
 GIT_SUBM	= $(shell \
@@ -86,9 +89,11 @@ sanitize	:
 			@echo "WARN: Compiling with fsanitize flag!"
 debug		:
 			@echo "WARN: debug is enabled"
+debug		:
+			@echo "WARN: debug_execution is enabled"
 noconnection:
 			@echo "WARN: Fetching submodules disabled!"
 
 .PHONY: \
  all clean fclean fcleanall re rmlibft\
- nWerror sanitize debug noconnection
+ nWerror sanitize debug debug_execution noconnection

includes/common.h
@@ -6,12 +6,19 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 02:36:55 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/09/02 15:00:37 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/09/02 15:14:24 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMON_H
 # define COMMON_H
+
+# ifndef KDEBUG
+#  define KDEBUG 0
+# endif
+# ifndef KDEBUG_EXECUTION
+#  define KDEBUG_EXECUTION 0
+# endif
 
 # define ERR_UNKNOWN			"Unknown error"
 

commit 559b1c13d452f3d3c78c28576dac3d6499cd4a6b

Fixes rule override and incorrect define name introduced in a418148ba9835c208efe5cce441daab503074257

Makefile
@@ -6,7 +6,7 @@
 #    By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/01/29 15:36:23 by ple-stra          #+#    #+#              #
-#    Updated: 2022/09/02 15:13:04 by ple-stra         ###   ########.fr        #
+#    Updated: 2022/09/02 15:35:46 by ple-stra         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -36,8 +36,8 @@ endif
 ifeq (debug, $(filter debug,$(MAKECMDGOALS)))
 	CFLAGS	+= -D KDEBUG=1
 endif
-ifeq (debug_execution, $(filter debug_execution,$(MAKECMDGOALS)))
-	CFLAGS	+= -D KDEBUG_EXECUTION=1
+ifeq (debug_exec, $(filter debug_exec,$(MAKECMDGOALS)))
+	CFLAGS	+= -D KDEBUG_EXEC=1
 endif
 
 GIT_SUBM	= $(shell \
@@ -89,11 +89,11 @@ sanitize	:
 			@echo "WARN: Compiling with fsanitize flag!"
 debug		:
 			@echo "WARN: debug is enabled"
-debug		:
-			@echo "WARN: debug_execution is enabled"
+debug_exec	:
+			@echo "WARN: debug_exec is enabled"
 noconnection:
 			@echo "WARN: Fetching submodules disabled!"
 
 .PHONY: \
  all clean fclean fcleanall re rmlibft\
- nWerror sanitize debug debug_execution noconnection
+ nWerror sanitize debug debug_exec noconnection

includes/common.h
@@ -6,18 +6,20 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 02:36:55 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/09/02 15:26:45 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/09/02 15:43:59 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMON_H
 # define COMMON_H
 
+# include "libft.h"
+
 # ifndef KDEBUG
 #  define KDEBUG 0
 # endif
-# ifndef KDEBUG_EXECUTION
-#  define KDEBUG_EXECUTION 0
+# ifndef KDEBUG_EXEC
+#  define KDEBUG_EXEC 0
 # endif
 
 # define ERR_UNKNOWN			"Unknown error"
@@ -58,6 +60,7 @@ typedef struct s_prg_data
 }	t_prg_data;
 
 int			execute(t_prg_data *prg_data);
+void		test_execution(t_prg_data *prg_data);
 
 t_command	default_command(void);
 