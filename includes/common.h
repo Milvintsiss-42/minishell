00ddadf52dce07d984527beb06f5e811fc93b030
ibadus <ibadus@icloud.com>
2022-09-03 19:11:26 +0200

    update: updated struct comments

--- includes/common.h
+++ includes/common.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   common.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 02:36:55 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/09/02 15:43:59 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/09/03 19:08:42 by oaarsse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,9 @@
 // -> stdout of cat is written to outfile2 in append mode
 // -> nothing is passed as stdin to the second "cat" but "cat" doesn't wait for
 // input.
-// 
+
+// Command stucture
+//
 // Each command needs to be initiated with default_command() method to avoid
 // unitialized parameters.
 typedef struct s_command
@@ -51,12 +53,19 @@ typedef struct s_command
 	int		is_last;
 }	t_command;
 
+// Initalized at startup
+// 
+// contains :
+// - program name
+// - environment variables
+// - the history of the commands
+// - amount of commands in the history
 typedef struct s_prg_data
 {
 	const char	*bin_name;
 	const char	**env;
-	t_command	*commands;
-	int			nb_commands;	
+	t_command	*h_commands;
+	int			nb_h_commands;
 }	t_prg_data;
 
 int			execute(t_prg_data *prg_data);
