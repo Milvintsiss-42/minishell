9b56abbd869d9f6cdf0fde8e213d22207bc32bd8
ibadus <ibadus@icloud.com>
2022-09-03 20:27:00 +0200

    init

---
+++ srcs/parsing/parsing.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 19:36:38 by oaarsse           #+#    #+#             */
+/*   Updated: 2022/09/03 20:26:53 by oaarsse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "common.h"
+#include <stdlib.h>
+
+void	parsing(t_prg_data *prg_data, char *line)
+{
+	t_command	cmd;
+
+	cmd = default_command();
+	return ;
+}
