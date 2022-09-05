b8d785df5c88f9cb58a58c19b0439bb35c7b5a9f
ibadus <ibadus@icloud.com>
2022-09-05 18:13:35 +0200

    wip parsing

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,17 +6,38 @@
 /*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 19:36:38 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/09/03 20:26:53 by oaarsse          ###   ########.fr       */
+/*   Updated: 2022/09/05 15:46:23 by oaarsse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
-#include <stdlib.h>
+#include "parsing.h"
 
+// TODO: check if all whitespaces are ignored in the command input
+// Checks for empty line
+int	is_empty(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (!ft_isspace(line[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+// Takes as input the raw user input from the shell
+// Will fill the program data struct with the commands to exec
+// Also checks if the line is not empty
 void	parsing(t_prg_data *prg_data, char *line)
 {
-	t_command	cmd;
+	t_command	*cmds;
 
-	cmd = default_command();
+	if (is_empty(line))
+		return ;
+	cmds = get_commands(line);
 	return ;
 }
