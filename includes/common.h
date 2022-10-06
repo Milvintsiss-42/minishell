efac9a108157758839a0bbec3e7786b945f20ee3
Milvintsiss <milvintsiss@gmail.com>
2022-10-06 02:32:33 +0200

    Launch a process for each command

--- includes/common.h
+++ includes/common.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   common.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 02:36:55 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/10/04 17:21:15 by oaarsse          ###   ########.fr       */
+/*   Updated: 2022/10/06 00:05:16 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,7 @@ typedef struct s_prg_data
 	const char	**env;
 	t_command	*commands;
 	int			nb_commands;
+	int			*commands_pids;
 
 	char		**history;
 	int			len_history;
