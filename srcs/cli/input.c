be6d2420c88346c3fc8e072b017033dd244a3fa5
ibadus <ibadus@icloud.com>
2022-10-07 15:46:40 +0200

    added history support

--- srcs/cli/input.c
+++ srcs/cli/input.c
@@ -6,13 +6,13 @@
 /*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 16:22:18 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/10/07 14:25:47 by oaarsse          ###   ########.fr       */
+/*   Updated: 2022/10/07 15:46:21 by oaarsse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 #include "parsing.h"
-#include "cli.h" 
+#include "cli.h"
 
 //TODO: handle if command is running
 //TODO: listen for signals CTRL + D or C or /
@@ -40,13 +40,15 @@ void	ft_loop_input(t_prg_data *data)
 		line = readline("> ");
 		//TODO: HANDLE ERROR if line is NULL
 		//TODO: add command to history
+		if (ft_strlen(line) > 0)
+			add_history(line);
 		(void)data;
-		(void)line;
 		(void)cmds;
 		// cmds = parsing(data, line);
 		// if (!cmds)
-			continue ; //TODO: handle error
+		// 	continue ; //TODO: handle error
 		//TODO: execution
 		//TODO: free allocated stuffs
+		free(line);
 	}
 }
