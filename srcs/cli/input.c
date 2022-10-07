ed3260726dd69ba2d25eab03ec270ef844e5e6dd
ibadus <ibadus@icloud.com>
2022-10-07 14:26:09 +0200

    removed parsing to have a functionnal without leaks version

--- srcs/cli/input.c
+++ srcs/cli/input.c
@@ -6,14 +6,13 @@
 /*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 16:22:18 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/10/07 14:23:22 by oaarsse          ###   ########.fr       */
+/*   Updated: 2022/10/07 14:25:47 by oaarsse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 #include "parsing.h"
 #include "cli.h" 
-
 
 //TODO: handle if command is running
 //TODO: listen for signals CTRL + D or C or /
@@ -41,8 +40,11 @@ void	ft_loop_input(t_prg_data *data)
 		line = readline("> ");
 		//TODO: HANDLE ERROR if line is NULL
 		//TODO: add command to history
-		cmds = parsing(data, line);
-		if (!cmds)
+		(void)data;
+		(void)line;
+		(void)cmds;
+		// cmds = parsing(data, line);
+		// if (!cmds)
 			continue ; //TODO: handle error
 		//TODO: execution
 		//TODO: free allocated stuffs
