b634dde5db13891ffc73cf1bc535c8fffe8684fd
Milvintsiss <milvintsiss@gmail.com>
2022-10-10 21:29:00 +0200

    Fixes command arguments array not being null terminated

--- srcs/execution/execution_testing.c
+++ srcs/execution/execution_testing.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 15:10:01 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/10/10 21:15:42 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/10/10 21:28:42 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,9 @@ void	test_execution(t_prg_data *prg_data)
 	prg_data->commands[1].args[0] = prg_data->commands[1].cmd;
 	prg_data->commands[2].args[0] = prg_data->commands[2].cmd;
 	prg_data->commands[2].args[1] = "-l";
+	prg_data->commands[0].args[1] = 0;
+	prg_data->commands[1].args[1] = 0;
+	prg_data->commands[2].args[2] = 0;
 	prg_data->commands[2].is_last = 1;
 	execute(prg_data);
 }
