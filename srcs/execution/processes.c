9ec3413fee2d554f271acbb509d4b9f7982b540d
Milvintsiss <milvintsiss@gmail.com>
2022-10-11 16:40:39 +0200

    Prints errors when happening

--- srcs/execution/processes.c
+++ srcs/execution/processes.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 19:13:36 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/10/11 16:33:43 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/10/11 16:40:14 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,13 +52,13 @@ int	launch_childs(t_prg_data *prg_data)
 
 	prg_data->commands_pids = malloc(sizeof(int) * prg_data->nb_commands);
 	if (!prg_data->commands_pids)
-		return (0);
+		return (ft_perror_errno(*prg_data) * 0);
 	i = 0;
 	while (i < prg_data->nb_commands)
 	{
 		prg_data->commands_pids[i] = fork();
 		if (prg_data->commands_pids < 0)
-			return (0);
+			return (ft_perror_errno(*prg_data) * 0);
 		if (prg_data->commands_pids[i] == 0)
 		{
 			launch_child(prg_data, &prg_data->commands[i]);
