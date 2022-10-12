1264896803ef3c4d905059ed6b983dba51bc58a2
Milvintsiss <milvintsiss@gmail.com>
2022-10-12 23:37:33 +0200

    Implements redirections

--- srcs/execution/redirections.c
+++ srcs/execution/redirections.c
@@ -6,22 +6,46 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 19:09:21 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/10/12 19:29:38 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/10/12 23:36:32 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
+#include <fcntl.h>
 
-// TODO
 void	set_infile_as_stdin(t_prg_data *prg_data, t_command *command)
 {
-	(void)prg_data;
-	(void)command;
+	int	fd_file;
+
+	fd_file = open(command->infile, O_RDONLY);
+	if (fd_file == -1)
+		exit_process(prg_data, command,
+			ft_fperror_errno(*prg_data, command->infile));
+	if (dup2(fd_file, STDIN_FILENO) == -1)
+	{
+		close(fd_file);
+		exit_process(prg_data, command, ft_perror_errno(*prg_data));
+	}
+	close(fd_file);
 }
 
-// TODO
 void	set_outfile_as_stdout(t_prg_data *prg_data, t_command *command)
 {
-	(void)prg_data;
-	(void)command;
+	int	fd_file;
+	int	oflag;
+
+	if (command->is_append_mode)
+		oflag = O_WRONLY | O_CREAT | O_APPEND;
+	else
+		oflag = O_WRONLY | O_CREAT | O_TRUNC;
+	fd_file = open(command->outfile, oflag, 0644);
+	if (fd_file == -1)
+		exit_process(prg_data, command,
+			ft_fperror_errno(*prg_data, command->outfile));
+	if (dup2(fd_file, STDOUT_FILENO) == -1)
+	{
+		close(fd_file);
+		exit_process(prg_data, command, ft_perror_errno(*prg_data));
+	}
+	close(fd_file);
 }
