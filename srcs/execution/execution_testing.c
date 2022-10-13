9d087bc6d1d5f08ac1e77949ddf6a28f1d82aec3
Milvintsiss <milvintsiss@gmail.com>
2022-10-13 16:56:41 +0200

    Updates text_execution to test here_docs

--- srcs/execution/execution_testing.c
+++ srcs/execution/execution_testing.c
@@ -6,39 +6,64 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 15:10:01 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/10/12 21:27:17 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/10/13 16:56:28 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 
+static t_command	test_command1(t_prg_data *prg_data)
+{
+	t_command	command;
+
+	command = default_command();
+	command.env = prg_data->env;
+	command.cmd = ft_strdup("cat");
+	command.args = malloc(sizeof(char *) * 2);
+	command.args[0] = ft_strdup(command.cmd);
+	command.args[1] = 0;
+	command.outfile = ft_strdup("outfile");
+	command.here_doc_limiter = ft_strdup("HERE");
+	return (command);
+}
+
+static t_command	test_command2(t_prg_data *prg_data)
+{
+	t_command	command;
+
+	command = default_command();
+	command.env = prg_data->env;
+	command.cmd = ft_strdup("echo");
+	command.args = malloc(sizeof(char *) * 3);
+	command.args[0] = ft_strdup(command.cmd);
+	command.args[1] = ft_strdup("hello world");
+	command.args[2] = 0;
+	return (command);
+}
+
+static t_command	test_command3(t_prg_data *prg_data)
+{
+	t_command	command;
+
+	command = default_command();
+	command.env = prg_data->env;
+	command.cmd = ft_strdup("cat");
+	command.args = malloc(sizeof(char *) * 2);
+	command.args[0] = ft_strdup(command.cmd);
+	command.args[1] = 0;
+	command.infile = ft_strdup("infile");
+	command.is_last = 1;
+	return (command);
+}
+
 // This execution is not protected as it is only used for debugging purposes
-// ls | cat | wc -l
+// cat << HERE > outfile | echo "hello world" | cat < infile
 int	test_execution(t_prg_data *prg_data)
 {
 	prg_data->nb_commands = 3;
 	prg_data->commands = malloc(sizeof(t_command) * prg_data->nb_commands);
-	prg_data->commands[0] = default_command();
-	prg_data->commands[0].env = prg_data->env;
-	prg_data->commands[0].cmd = ft_strdup("ls");
-	prg_data->commands[0].args = malloc(sizeof(char *) * 2);
-	prg_data->commands[0].args[0] = ft_strdup(prg_data->commands[0].cmd);
-	prg_data->commands[0].args[1] = 0;
-	prg_data->commands[0].outfile = ft_strdup("outfile");
-	prg_data->commands[1] = default_command();
-	prg_data->commands[1].env = prg_data->env;
-	prg_data->commands[1].cmd = ft_strdup("echo");
-	prg_data->commands[1].args = malloc(sizeof(char *) * 3);
-	prg_data->commands[1].args[0] = ft_strdup(prg_data->commands[1].cmd);
-	prg_data->commands[1].args[1] = ft_strdup("hello world");
-	prg_data->commands[1].args[2] = 0;
-	prg_data->commands[2] = default_command();
-	prg_data->commands[2].env = prg_data->env;
-	prg_data->commands[2].cmd = ft_strdup("cat");
-	prg_data->commands[2].args = malloc(sizeof(char *) * 2);
-	prg_data->commands[2].args[0] = ft_strdup(prg_data->commands[2].cmd);
-	prg_data->commands[2].args[1] = 0;
-	prg_data->commands[2].infile = ft_strdup("infile");
-	prg_data->commands[2].is_last = 1;
+	prg_data->commands[0] = test_command1(prg_data);
+	prg_data->commands[1] = test_command2(prg_data);
+	prg_data->commands[2] = test_command3(prg_data);
 	return (execute(prg_data));
 }
