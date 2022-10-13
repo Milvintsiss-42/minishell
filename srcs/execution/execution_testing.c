437e12aabd7db588e90ed1b2713abd259038d151
Milvintsiss <milvintsiss@gmail.com>
2022-10-13 18:10:54 +0200

    Updates text_execution to test here_docs

--- srcs/execution/execution_testing.c
+++ srcs/execution/execution_testing.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 15:10:01 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/10/13 16:56:28 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/10/13 18:09:22 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,14 +56,29 @@ static t_command	test_command3(t_prg_data *prg_data)
 	return (command);
 }
 
+static t_command	test_command4(t_prg_data *prg_data)
+{
+	t_command	command;
+
+	command = default_command();
+	command.env = prg_data->env;
+	command.cmd = ft_strdup("cat");
+	command.args = malloc(sizeof(char *) * 2);
+	command.args[0] = ft_strdup(command.cmd);
+	command.args[1] = 0;
+	command.here_doc_limiter = ft_strdup("HERE2");
+	return (command);
+}
+
 // This execution is not protected as it is only used for debugging purposes
-// cat << HERE > outfile | echo "hello world" | cat < infile
+// cat << HERE > outfile | echo "hello world" | cat < infile | cat <<HERE2
 int	test_execution(t_prg_data *prg_data)
 {
-	prg_data->nb_commands = 3;
+	prg_data->nb_commands = 4;
 	prg_data->commands = malloc(sizeof(t_command) * prg_data->nb_commands);
 	prg_data->commands[0] = test_command1(prg_data);
 	prg_data->commands[1] = test_command2(prg_data);
 	prg_data->commands[2] = test_command3(prg_data);
+	prg_data->commands[3] = test_command4(prg_data);
 	return (execute(prg_data));
 }
