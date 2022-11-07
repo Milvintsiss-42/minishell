63b77f86e7cca687b77fd3deb8b940adda207b81
ibadus debian <ibaus@icloud.com>
2022-11-07 16:58:23 +0100

    refacto parsing to clean it

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 17:20:47 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/11/07 15:27:33 by oaarsse          ###   ########.fr       */
+/*   Updated: 2022/11/07 16:46:08 by oaarsse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,17 @@ t_command	*parsing(t_prg_data *prog_data, char *line)
 	t_command			*commands;
 	t_lst_tokens		*tokens;
 
+	if (!line || *line == '\0')
+		return (NULL);
+	if (validate_input(line) == -1)
+		return (NULL);
 	tokens = tokenizer(line);
 	if (!tokens)
-		return (NULL); // tokens is freed in tokenizer on error
-	// TODO: add handling if tokens is empty (i.e only spaces were entered)
-	t_lst_tokens *tmp = tokens;
-	while (tmp)
-	{
-		printf("TOKENS [%s]\n", tmp->token);
-		tmp = tmp->next;
-	}
+		return (NULL);
 	tokens = lexer(tokens, prog_data);
-	if (tokens == NULL)
-		return (NULL); // tokens is freed in lexer on error
-	t_lst_tokens *tmp2 = tokens;
-	while (tmp2)
-	{
-		printf("LEXER [%s]\n", tmp2->token);
-		tmp2 = tmp2->next;
-	}
-	//TODO: PARSE LEXER TO COMMANDS
-	free_tokenizer(tokens);
-	commands = NULL;
+	if (!tokens)
+		return (NULL);
+	commands = cmd_translator(tokens, prog_data);
+	free_parsing(tokens);
 	return (commands);
 }
