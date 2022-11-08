35077576c504837a36c9db9a540fb05804d72dd5
Milvintsiss <milvintsiss@gmail.com>
2022-11-08 18:32:22 +0100

    Fixes string not null terminated

--- srcs/execution/builtins/echo_builtin.c
+++ srcs/execution/builtins/echo_builtin.c
@@ -6,39 +6,59 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 19:01:28 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/11/07 22:58:32 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/11/08 18:31:36 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-static int	get_output_from_args(char **r_output,
-		t_prg_data *prg_data, char **args, int add_newline)
+static int	get_output_len(char **args)
 {
-	char	*output;
+	int		i;
 	size_t	output_len;
+
+	i = 0;
+	output_len = 0;
+	while (args[i] != 0)
+	{
+		output_len += ft_strlen(args[i]) + (i != 0);
+		i++;
+	}
+	return (output_len);
+}
+
+static char	*join_output(char *output, char **args, int add_newline)
+{
 	size_t	cur_output_len;
 	int		i;
 	size_t	arg_len;
 
-	i = -1;
-	output_len = 0;
-	while (args[++i] != 0)
-		output_len += ft_strlen(args[i]);
-	output = malloc(sizeof(char) * (output_len + add_newline + 1));
-	if (!output)
-		return (ft_perror_errno(*prg_data));
-	i = -1;
-	cur_output_len = 1;
-	while (args[++i] != 0)
+	*output = 0;
+	i = 0;
+	cur_output_len = 0;
+	while (args[i] != 0)
 	{
 		arg_len = ft_strlen(args[i]);
-		cur_output_len += arg_len;
-		ft_strlcat(output, args[i], cur_output_len);
+		cur_output_len += arg_len + (i != 0);
+		if (i != 0)
+			ft_strlcat(output, " ", cur_output_len + 1);
+		ft_strlcat(output, args[i], cur_output_len + 1);
+		i++;
 	}
 	if (add_newline)
-		ft_strlcat(output, "\n", cur_output_len + 1);
-	*r_output = output;
+		ft_strlcat(output, "\n", cur_output_len + 2);
+	return (output);
+}
+
+static int	get_output_from_args(char **r_output,
+		t_prg_data *prg_data, char **args, int add_newline)
+{
+	char	*output;
+
+	output = malloc(sizeof(char) * (get_output_len(args) + add_newline + 1));
+	if (!output)
+		return (ft_perror_errno(*prg_data));
+	*r_output = join_output(output, args, add_newline);
 	return (0);
 }
 
