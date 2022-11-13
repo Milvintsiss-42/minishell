cab10e1ed3b4a1ca14e1f99270456031ed8c01f6
Milvintsiss <milvintsiss@gmail.com>
2022-11-14 00:00:49 +0100

    Switches to home directory when using cd builtin with no arguments

--- srcs/execution/builtins/cd_builtin.c
+++ srcs/execution/builtins/cd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 19:01:28 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/11/13 21:43:45 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/11/13 23:58:28 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,38 @@ int	updates_env_pwd(t_prg_data *prg_data)
 	return (rstatus);
 }
 
+static int	set_current_directory_to_home(t_prg_data *prg_data)
+{
+	char	*home_dir;
+
+	if (get_env_element_address_by_name(prg_data, "HOME") == 0)
+		return (ft_fperror(*prg_data, "cd", "HOME not set"));
+	home_dir = get_env_element_value_by_name(prg_data, "HOME");
+	if (!home_dir)
+		return (ft_perror_errno(*prg_data));
+	if (chdir(home_dir) == -1)
+	{
+		free(home_dir);
+		return (ft_perror_errno(*prg_data));
+	}
+	free(home_dir);
+	return (0);
+}
+
 int	exec_cd_builtin(t_prg_data *prg_data, t_command *command)
 {
-	if (chdir(command->args[1]) == -1)
-		return (ft_perror_errno(*prg_data) * 0);
+	int	rstatus;
+
+	if (command->args[1] == 0)
+	{
+		rstatus = set_current_directory_to_home(prg_data);
+		if (rstatus != 0)
+			return (rstatus);
+	}
+	else
+	{
+		if (chdir(command->args[1]) == -1)
+			return (ft_perror_errno(*prg_data) * 0);
+	}
 	return (updates_env_pwd(prg_data));
 }
