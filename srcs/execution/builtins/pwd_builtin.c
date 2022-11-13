e3bd3b71069bdfcadad410c32362f013e8a816c8
Milvintsiss <milvintsiss@gmail.com>
2022-11-13 03:41:09 +0100

    Updates libft to latest

--- srcs/execution/builtins/pwd_builtin.c
+++ srcs/execution/builtins/pwd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 20:05:31 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/11/07 21:34:59 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/11/13 04:27:28 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	exec_pwd_builtin(t_prg_data *prg_data, t_command *command)
 	buf = malloc(sizeof(char) * 500);
 	if (!getcwd(buf, sizeof(char) * 500))
 	{
-		buf = ft_realloc(buf, sizeof(char) * 2048);
+		buf = ft_realloc(buf, sizeof(char) * 500, sizeof(char) * 2048);
 		if (!getcwd(buf, sizeof(char) * 2048))
 			return (ft_perror_errno(*prg_data) * 0);
 	}
