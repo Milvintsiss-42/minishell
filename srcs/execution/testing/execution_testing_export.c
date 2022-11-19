d6368593f654a30bdeae30fbfd83e65701e9b6d6
Milvintsiss <milvintsiss@gmail.com>
2022-11-19 03:14:33 +0100

    Adds tests to unset the last env element

--- srcs/execution/testing/execution_testing_export.c
+++ srcs/execution/testing/execution_testing_export.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 02:38:38 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/11/14 02:57:53 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/11/19 03:01:31 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	test_execution_export(t_prg_data *prg_data)
 	prg_data->commands = malloc(sizeof(t_command) * prg_data->nb_commands);
 	prg_data->commands[0] = default_command();
 	prg_data->commands[0].cmd = ft_strdup("export");
-	prg_data->commands[0].args = malloc(sizeof(char *) * 8);
+	prg_data->commands[0].args = malloc(sizeof(char *) * 9);
 	prg_data->commands[0].args[0] = ft_strdup(prg_data->commands[0].cmd);
 	prg_data->commands[0].args[1] = ft_strdup("new=");
 	prg_data->commands[0].args[2] = ft_strdup("new2=a");
@@ -26,7 +26,8 @@ int	test_execution_export(t_prg_data *prg_data)
 	prg_data->commands[0].args[4] = ft_strdup("new3==hello equal sign");
 	prg_data->commands[0].args[5] = ft_strdup("=i'm empty bitch");
 	prg_data->commands[0].args[6] = ft_strdup("new 5=and an invalid one");
-	prg_data->commands[0].args[7] = 0;
+	prg_data->commands[0].args[7] = ft_strdup("last=this is the last one");
+	prg_data->commands[0].args[8] = 0;
 	prg_data->commands[0].is_last = 1;
 	return (execute(prg_data));
 }
