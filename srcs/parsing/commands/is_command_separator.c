d277e8d1cde7fe739fa7d9f1aab3c5c545719282
Milvintsiss <milvintsiss@gmail.com>
2022-11-19 22:00:44 +0100

    Fixes Pipe not being recognized as command separator

--- srcs/parsing/commands/is_command_separator.c
+++ srcs/parsing/commands/is_command_separator.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_command_separator.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 18:05:27 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/11/10 15:34:03 by oaarsse          ###   ########.fr       */
+/*   Updated: 2022/11/19 21:58:47 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_token_separator	is_or_and_pipe(char *str)
 		return (e_OR);
 	else if (str && *str && *(str + 1) && ft_strncmp(str, STR_AND, 2) == 0)
 		return (e_AND);
-	else if (str && *str && *(str + 1) && ft_strncmp(str, STR_PIPE, 1) == 0)
+	else if (str && *str && ft_strncmp(str, STR_PIPE, 1) == 0)
 		return (e_PIPE);
 	return (e_NONE);
 }
