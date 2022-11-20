402787fd434c2929438a849afdaff8e24fa43635
Milvintsiss <milvintsiss@gmail.com>
2022-11-20 02:50:36 +0100

    Adds a custom prompt showing the current directory

--- srcs/cli/input.c
+++ srcs/cli/input.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 16:22:18 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/11/17 23:47:54 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/11/20 02:40:56 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,41 @@ static void	handle_parsing_exec(t_prg_data *data, char	*line)
 	cmd_no++;
 }
 
+static char	*get_prompt(t_prg_data *data)
+{
+	char	*prompt;
+	char	*pwd;
+	char	*pwd_basename;
+
+	pwd = get_env_element_value_by_name(data, "PWD");
+	if (!pwd)
+		return (0);
+	pwd_basename = ft_basename(pwd);
+	free(pwd);
+	if (!pwd_basename)
+		return (0);
+	ft_vasprintf(&prompt, "-> %s$ ", pwd_basename);
+	free(pwd_basename);
+	return (prompt);
+}
+
 // ask indefinitely for a command
 // each command input is parsed and executed
 //TODO: listen for signals CTRL + D or C or /
 void	ft_loop_input(t_prg_data *data)
 {
 	char		*line;
+	char		*prompt;
 
 	ft_signal_handler();
 	while (TRUE)
 	{
-		line = readline("> ");
+		prompt = get_prompt(data);
+		if (prompt)
+			line = readline(prompt);
+		else
+			line = readline("$ ");
+		free(prompt);
 		if (!line)
 		{
 			// CTRL + D case
