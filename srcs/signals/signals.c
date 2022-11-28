3d88cabb691839d31696cc4e6aaf51dd5cdc5e8c
ibadus <ibaus@icloud.com>
2022-11-28 20:48:31 +0100

    added some more signal handlers (wip)

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 18:09:06 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/11/25 00:34:44 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/11/28 19:23:57 by oaarsse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	signal_int(int signo)
 {
 	if (signo == SIGINT)
 	{
-		g_last_exit_status = 1;
+		g_last_exit_status = 130;
 		ft_putstr_fd("\n", 2);
 		rl_replace_line("", 0);
 		rl_on_new_line();
@@ -30,4 +30,28 @@ void	ft_signal_handler(void)
 {
 	signal(SIGINT, signal_int);
 	signal(SIGQUIT, SIG_IGN);
+}
+
+static void	signal_int_heredoc(int signo)
+{
+	if (signo == SIGINT)
+	{
+		g_last_exit_status = 130;
+		close(0);
+		return ;
+	}
+}
+
+static void	signal_quit_heredoc(int signo)
+{
+	if (signo == SIGQUIT)
+	{
+		signal(SIGQUIT, SIG_IGN);
+	}
+}
+
+void	ft_signal_handler_heredoc(void)
+{
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, signal_int_heredoc);
 }
