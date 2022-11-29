209135235a121ee8c5232e042003ae74c1b86c1a
ibadus <ibaus@icloud.com>
2022-11-29 22:19:29 +0100

    fixed bug where sep where considered as command

--- srcs/parsing/commands/cmd_generator.c
+++ srcs/parsing/commands/cmd_generator.c
@@ -3,36 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_generator.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:30:26 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/11/26 01:20:34 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/11/29 22:16:41 by oaarsse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
-
-static int	initalize_cmd(t_command	*command, t_lst_tokens **tokens)
-{
-	if (is_separator((*tokens)->token) == e_OPEN_PRTH)
-	{
-		command->is_opening_parenthesis = 1;
-		*tokens = (*tokens)->next;
-	}
-	command->args = (char **)malloc(sizeof(char *) * 2);
-	if (!command->args)
-		return (-1);
-	command->args[0] = ft_strdup((*tokens)->token);
-	if (!command->args[0])
-		return (-1);
-	command->args[1] = NULL;
-	command->is_expandable = (t_bool *)malloc(sizeof(t_bool) * 1);
-	if (!command->is_expandable)
-		return (-1);
-	command->is_expandable[0] = (*tokens)->does_expand;
-	*tokens = (*tokens)->next;
-	return (1);
-}
 
 static int	final_touch(t_command	*command, t_lst_tokens **tokens,
 		t_token_separator sep)
@@ -53,8 +31,6 @@ int	command_generator(t_command	*command, t_lst_tokens **tokens)
 {
 	t_token_separator	sep;
 
-	if (!initalize_cmd(command, tokens))
-		return (-1);
 	sep = 0;
 	while (*tokens)
 	{
