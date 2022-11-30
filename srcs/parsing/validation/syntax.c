725c2e283b3f87e3f421cc46debff24a10ead47b
oaarsse <oaarsse@student.42.fr>
2022-11-30 14:00:24 +0100

    allowed (redirec) + separator redirect

--- srcs/parsing/validation/syntax.c
+++ srcs/parsing/validation/syntax.c
@@ -6,7 +6,7 @@
 /*   By: oaarsse <oaarsse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:03:50 by oaarsse           #+#    #+#             */
-/*   Updated: 2022/11/29 23:42:47 by oaarsse          ###   ########.fr       */
+/*   Updated: 2022/11/30 14:00:21 by oaarsse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 t_bool	is_valid_suite(t_lst_tokens *token)
 {
+	if (is_separator(token->token) == e_CLOSE_PRTH && token->next
+		&& is_separator(token->next->token) == e_OPEN_PRTH)
+		return (TRUE);
 	if (is_separator(token->token)
 		&& !(is_separator(token->token) == e_RD_FILE
 			|| is_separator(token->token) == e_RD_STDIN
@@ -32,10 +35,15 @@ t_bool	is_invalid(t_lst_tokens *token)
 			|| is_separator(token->token) == e_RD_STDIN
 			|| is_separator(token->token) == e_RD_FILE_APN
 			|| is_separator(token->token) == e_RD_STDIN_HEREDOC)
-		&& token->next && is_separator(token->next->token) == e_OPEN_PRTH)
+		&& token->next && is_separator(token->next->token)
+		&& is_separator(token->next->token) != e_CLOSE_PRTH)
 		return (TRUE);
 	if ((token->next && is_separator(token->next->token)
-			&& is_separator(token->next->token) != e_OPEN_PRTH)
+			&& is_separator(token->next->token) != e_OPEN_PRTH
+			&& !(is_separator(token->next->token) == e_RD_FILE
+				|| is_separator(token->next->token) == e_RD_STDIN
+				|| is_separator(token->next->token) == e_RD_FILE_APN
+				|| is_separator(token->next->token) == e_RD_STDIN_HEREDOC))
 		&& is_separator(token->token) != e_CLOSE_PRTH)
 		return (TRUE);
 	if (is_separator(token->token) == e_OPEN_PRTH
@@ -43,9 +51,6 @@ t_bool	is_invalid(t_lst_tokens *token)
 		return (TRUE);
 	if (is_separator(token->token) == e_OPEN_PRTH && token->prev
 		&& !is_separator(token->prev->token))
-		return (TRUE);
-	if (is_separator(token->token) == e_CLOSE_PRTH && token->next
-		&& is_separator(token->next->token) == e_OPEN_PRTH)
 		return (TRUE);
 	return (is_valid_suite(token));
 }
