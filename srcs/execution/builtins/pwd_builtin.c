f0a12a8102d67a32ffb629ac9a0ac1efd74fcaf4
Milvintsiss <milvintsiss@gmail.com>
2022-12-01 03:46:44 +0100

    Fixes pwd builtin reporting an Unknown Error

--- srcs/execution/builtins/pwd_builtin.c
+++ srcs/execution/builtins/pwd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 20:05:31 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/12/01 02:25:26 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/12/01 03:44:05 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ int	exec_pwd_builtin(t_prg_data *prg_data, t_command *command)
 
 	err = 0;
 	(void)command;
+	if (get_env_element_address_by_name(prg_data, "PWD") == 0)
+	{
+		if (updates_env_pwd(prg_data, FALSE) != 0)
+			return (errno);
+	}
 	pwd = get_env_element_value_by_name(prg_data, "PWD");
 	if (!pwd)
 		return (ft_perror_errno(*prg_data));
