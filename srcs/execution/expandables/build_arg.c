76e8d6cd94cc855884ebe09bb1f512f799ab27cb
Milvintsiss <milvintsiss@gmail.com>
2022-12-01 02:54:00 +0100

    Fixes build_arg not returning on error

--- srcs/execution/expandables/build_arg.c
+++ srcs/execution/expandables/build_arg.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 18:13:35 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/11/28 18:13:54 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/12/01 02:53:05 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,10 @@ size_t	get_arg_len(t_arg_cpnt *arg_cpnts)
 	return (t_len);
 }
 
-char	*build_arg(t_prg_data *prg_data, t_arg_cpnt *arg_cpnts)
+static void	cpy_components_to_arg(t_arg_cpnt *arg_cpnts, char *arg)
 {
-	char		*arg;
-	char		*arg_save;
-	t_arg_cpnt	*arg_cpnts_save;
-	char		*cpnt;
-	size_t		t_len;
+	char	*cpnt;
 
-	arg_cpnts_save = arg_cpnts;
-	t_len = get_arg_len(arg_cpnts);
-	arg = malloc(sizeof(char) * (t_len + 1));
-	if (!arg)
-	{
-		ft_perror_errno(*prg_data);
-		free_cpnts(arg_cpnts_save);
-	}
-	arg_save = arg;
 	while (arg_cpnts)
 	{
 		cpnt = arg_cpnts->component;
@@ -51,6 +38,22 @@ char	*build_arg(t_prg_data *prg_data, t_arg_cpnt *arg_cpnts)
 		arg_cpnts = arg_cpnts->next;
 	}
 	*arg = 0;
-	free_cpnts(arg_cpnts_save);
-	return (arg_save);
+}
+
+char	*build_arg(t_prg_data *prg_data, t_arg_cpnt *arg_cpnts)
+{
+	char		*arg;
+	size_t		t_len;
+
+	t_len = get_arg_len(arg_cpnts);
+	arg = malloc(sizeof(char) * (t_len + 1));
+	if (!arg)
+	{
+		free_cpnts(arg_cpnts);
+		ft_perror_errno(*prg_data);
+		return (0);
+	}
+	cpy_components_to_arg(arg_cpnts, arg);
+	free_cpnts(arg_cpnts);
+	return (arg);
 }
